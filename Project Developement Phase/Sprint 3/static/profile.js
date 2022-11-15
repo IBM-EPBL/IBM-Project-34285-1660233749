6056f06fa291c7f02d2794b5cace4b275a98d5f1
santhosh <96367822+santhoshmg160@users.noreply.github.com>
2022-11-15 11:29:16 +0530

    Add files via upload

---
+++ Project Developement Phase/Sprint 3/static/profile.js
@@ -0,0 +1,18 @@
+// grab everything we need
+let profileEl = document.getElementById("profile")
+let formEl = document.getElementById("profile-form")
+let editEl = document.getElementById("editBtn")
+
+editEl.addEventListener("click", () => {
+    profileEl.classList.add("hidden");
+    formEl.classList.remove("hidden");
+})
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitH
